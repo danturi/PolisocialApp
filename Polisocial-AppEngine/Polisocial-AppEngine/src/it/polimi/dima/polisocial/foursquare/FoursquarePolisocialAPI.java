785cf81b0c669406c65f966ca4e99765eb7aa6aa
danturi <danturi89@gmail.com>
2014-07-29 04:03:53 +0200

    fix TokenRequest now WORK

--- Polisocial-AppEngine/Polisocial-AppEngine/src/it/polimi/dima/polisocial/foursquare/FoursquarePolisocialAPI.java
+++ Polisocial-AppEngine/Polisocial-AppEngine/src/it/polimi/dima/polisocial/foursquare/FoursquarePolisocialAPI.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import javax.xml.ws.http.HTTPException;
+
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiMethod.HttpMethod;
@@ -19,6 +21,7 @@ import com.google.api.server.spi.config.ApiNamespace;
 import com.google.api.server.spi.config.Named;
 import com.google.appengine.labs.repackaged.org.json.JSONArray;
 import com.google.appengine.labs.repackaged.org.json.JSONException;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
 import com.google.appengine.labs.repackaged.org.json.JSONTokener;
 
 import fi.foyt.foursquare.api.FoursquareApi;
@@ -36,10 +39,10 @@ public class FoursquarePolisocialAPI {
 	private List<String> venuesName = new ArrayList<String>();
 	
 	@ApiMethod(name = "searchVenues")
-	public StringObjectCollection searchVenues(@Named("ll")String ll) throws FoursquareApiException {
+	public StringObjectCollection searchVenues(@Named("ll")String ll) throws FoursquareApiException  {
 
 		// Coordinate Politecnico di Milano
-		 ll = "45.478178,9.228031";
+		 //ll = "45.478178,9.228031";
 
 		// Categorie cibo
 		String categoryIds = "4bf58dd8d48988d143941735,52e81612bcbc57f1066b79f4,4bf58dd8d48988d16c941735,"
@@ -50,50 +53,53 @@ public class FoursquarePolisocialAPI {
 		
 		// First we need a initialize FoursquareApi. 
 		FoursquareApi foursquareApi = new FoursquareApi(Constants.CLIENT_ID, Constants.CLIENT_SECRET, Constants.CALLBACK_URL2);
-		Result<VenuesSearchResult> result = foursquareApi.venuesSearch(coordinates, null, null, null, null, null, "browse",categoryIds, null, null, null,800 , null);
-		
-		StringObjectCollection venuesListObj = new StringObjectCollection();
-		log.warning("codice:" + result.getMeta().getCode());
+		Result<VenuesSearchResult> result;
 
-		if (result.getMeta().getCode() == 200) {
+			result = foursquareApi.venuesSearch(coordinates, null, null, null, null, null, "browse",categoryIds, null, null, null,800 , null);
+			StringObjectCollection venuesListObj = new StringObjectCollection();
 
-			// if query was ok we can finally we do something with the data
+			if (result.getMeta().getCode() == 200) {
 
-			for (CompactVenue venue : result.getResult().getVenues())
-				venuesName.add(venue.getName());
-		venuesListObj.setStringList(venuesName);
-		return venuesListObj;
-		} else {
-			if (result.getMeta().getCode() == 404) throw new FoursquareApiException("nessun risultato");
-			if (result.getMeta().getCode() == 500) throw new FoursquareApiException("errore del server");
+				// if query was ok we can finally we do something with the data
+
+				for (CompactVenue venue : result.getResult().getVenues())
+					venuesName.add(venue.getName());
+			venuesListObj.setStringList(venuesName);
 			return venuesListObj;
-		}
+			} else {
+				return venuesListObj;
+			}
+		
+
 	}
 	
 	@ApiMethod(name= "performTokenRequest", httpMethod = HttpMethod.GET)
 	public void performTokenRequest(@Named("code") String code){
 		log.info("richiesta token");
 		try {
-			URL url = new URL("https://foursquare.com/oauth2/access_token?client_id="+Constants.CLIENT_ID+"&client_secret="+Constants.CLIENT_SECRET+
-					"&grant_type=authorization_code&redirect_uri="+Constants.CALLBACK_URL2+"&code="+code);
- 
-			HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-			connection.setRequestMethod("GET");
-			connection.connect();
-			if (connection.getResponseCode() == 200){
-			//BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-			JSONTokener tokener = new JSONTokener(connection.getContent().toString());
-			JSONArray finalResult = new JSONArray(tokener);
-			log.info(finalResult.toString());
-			}else{
-				log.info(connection.getResponseCode()+connection.getResponseMessage());
-			}
+			String baseUrl= "https://foursquare.com/oauth2/access_token?client_id=";
+			StringBuilder urlBuilder = new StringBuilder(baseUrl);
+			urlBuilder.append(Constants.CLIENT_ID);
+			urlBuilder.append("&client_secret="+Constants.CLIENT_SECRET);
+			urlBuilder.append("&grant_type=authorization_code&code="+code);
+			String url = urlBuilder.toString();
+			log.info(url);
+			
+			URL tokenUrl = new URL(url);
+			//https://foursquare.com/oauth2/access_token?client_id=C0UAYKHQET5QIKKQY50WOMCR50BESMVBGAN1BR5NSEHJ4NKU&client_secret=2RYQF34SHI2IGOETK1RS4PYTCMBHXODSZXJMD1SH4WRKQGCD&grant_type=authorization_code&code=
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(tokenUrl.openStream()));
+			JSONObject object = (JSONObject) new JSONTokener(reader.readLine()).nextValue();
+			String token = object.getString("access_token");
+			log.info(token);
+			
 		} catch (MalformedURLException e) {
 			log.warning(e.getMessage());
 		} catch (IOException e) {
 			log.warning(e.getMessage());
 		} catch (JSONException e) {
-			log.warning(e.getMessage());
+			log.info(e.getCause().toString());
+			log.info(e.getMessage());
 		}
 		
 		
